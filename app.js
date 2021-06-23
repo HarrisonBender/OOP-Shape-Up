bf2ff52b500e771fede10e217f318c9c985a7130
HarrisonBender <83611772+HarrisonBender@users.noreply.github.com>
2021-06-23 12:44:15 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,165 @@
+const MAX = 601;
+let container = document.getElementById("container");
+let info = document.getElementById("info");
+let selShape = document.getElementById("sel-shape");
+let selWidth = document.getElementById("sel-width");
+let selHeight = document.getElementById("sel-height");
+let selArea = document.getElementById("sel-area");
+let selPerim = document.getElementById("sel-perimeter");
+let selRadius = document.getElementById("sel-radius");
+
+// Math for placement
+function randomVal(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
+}
+
+//Shape parent Class
+class Shape {
+    constructor(height, width) {
+        this.height = height;
+        this.width = width;
+        //draws the shapes into the containing div
+        this.div = document.createElement('div');
+        this.div.classList.add('shape');
+        container.appendChild(this.div);
+    }
+    //Puts a shape in a random location
+    randomLocation() {
+        let xVal = randomVal(this.width, MAX);
+        let yVal = randomVal(this.height, MAX);
+        this.div.style.top = `${yVal}px`;
+        this.div.style.left = `${xVal}px`;
+    }
+    //Describe shape when clicked
+    describe(id, height, width) {
+        selShape.innerHTML = `Selected Shape: ${id}`;
+        selWidth.innerHTML = `Width: ${width}`;
+        selHeight.innerHTML = `Height: ${height}`;
+    }
+    //Removes the shape from the container
+    remove() {
+        container.removeChild(this.div);
+        selShape.innerHTML = `Selected Shape: `;
+        selWidth.innerHTML = `Width: `;
+        selHeight.innerHTML = `Height: `;
+        selArea.innerHTML = `Area: `;
+        selPerim.innerHTML = `Perimeter: `;
+    }
+}
+
+class Rectangle extends Shape {
+    constructor(height, width) {
+        super(height, width);
+        this.div.id = "rectangle";
+        this.div.style.height = `${height}px`;
+        this.div.style.width = `${width}px`;
+        this.randomLocation();
+        this.div.addEventListener("click", () => {
+            this.describe(this.div.id, this.height, this.width);
+            selArea.innerHTML = `Area: ${height * width}`;
+            selPerim.innerHTML = `Perimeter: ${(height * 2) + (width * 2)}`;
+        });
+        this.div.addEventListener("dblclick", () => {
+            this.remove();
+        });
+    }
+}
+
+class Square extends Shape {
+    constructor(sideLength) {
+        super(sideLength, sideLength);
+        this.sideLength = sideLength;
+        this.div.id = "square";
+        this.div.style.height = `${sideLength}px`;
+        this.div.style.width = `${sideLength}px`;
+        this.randomLocation();
+        this.div.addEventListener("click", () => {
+            this.describe(this.div.id, this.sideLength, this.sideLength);
+            selArea.innerHTML = `Area: ${sideLength * sideLength}`;
+            selPerim.innerHTML = `Perimeter: ${sideLength * 4}`;
+        });
+        this.div.addEventListener("dblclick", () => {
+            this.remove();
+        });
+    }
+}
+
+class Circle extends Shape {
+    constructor(radius) {
+        super(radius, radius);
+        this.radius = radius;
+        this.div.id = "circle";
+        this.div.style.height = `${radius}px`;
+        this.div.style.width = `${radius}px`;
+        this.randomLocation();
+        this.div.addEventListener("click", () => {
+            this.describe(this.div.id, this.radius, this.radius);
+            selWidth.innerHTML = `Diameter: ${radius * 2}`;
+            selHeight.innerHTML = `Radius: ${radius}`;
+            selArea.innerHTML = `Area: ${(radius * radius) * Math.PI}`;
+            selPerim.innerHTML = `Circumfrence: ${2 * Math.PI * radius}`;
+        });
+        this.div.addEventListener("dblclick", () => {
+            this.remove();
+        });
+    }
+}
+
+class Triangle extends Shape {
+    constructor(height) {
+        super(height, height);
+        this.div.id = "triangle";
+        this.div.style.borderBottom = `${height}px solid #fff000`;
+        this.div.style.borderRight = `${height}px solid transparent`;
+        this.randomLocation();
+        this.div.addEventListener("click", () => {
+            this.describe(this.div.id, this.height, this.width);
+            selArea.innerHTML = `Area: ${0.5 * height * height}`;
+            selPerim.innerHTML = `Perimeter: ${2 * height + Math.sqrt(2) * height}`;
+        });
+        this.div.addEventListener("dblclick", () => {
+            this.remove();
+        });
+    }
+}
+//Rectange Button
+let btnRectangle = document.getElementById("rectangle-button");
+btnRectangle.addEventListener("click", function () {
+    let rect = document.getElementById("rectangle-text").value;
+    let rectArray = rect.split(" ");
+    if (rectArray[0] >= 601 || rectArray[1] >= 601) {
+        alert("Enter a value less than 601");
+    } else {
+        let sh = new Rectangle(rectArray[0], rectArray[1]);
+    }
+});
+//Square Button
+let btnSquare = document.getElementById("square-button");
+btnSquare.addEventListener("click", function () {
+    let sq = document.getElementById("square-text").value;
+    if (sq >= 601) {
+        alert("Enter a value less than 601");
+    } else {
+        let squ = new Square(sq);
+    }
+});
+//Circle Button
+let btnCircle = document.getElementById("circle-button");
+btnCircle.addEventListener("click", function () {
+    let cir = document.getElementById("circle-text").value;
+    if (cir >= 601) {
+        alert("Enter a value less than 601");
+    } else {
+        let circ = new Circle(cir);
+    }
+});
+//Triange Button
+let btnTriangle = document.getElementById("triangle-button");
+btnTriangle.addEventListener("click", function () {
+    let tri = document.getElementById("triangle-text").value;
+    if (tri >= 601) {
+        alert("Enter a value less than 601");
+    } else {
+        let triang = new Triangle(tri);
+    }
+});
